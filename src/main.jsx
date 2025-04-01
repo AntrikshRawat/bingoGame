c3af23fd5ead103ec26a6a96476bcac395a91866
Antriksh rawat <antrikshrawat2@gmail.com>
2025-04-01 23:37:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./index.css"
 
 createRoot(document.getElementById('root')).render(
     <>
     <Analytics/>
-    <SpeedInsights/>
     <App />
     </>
 )
